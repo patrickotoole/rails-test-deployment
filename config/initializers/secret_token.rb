586548503481e11db28ad92fd8f10940cfbeca8c
Your Name <rick.otoole@snet.net>
2011-04-04 17:03:22 -0400

    test app for testing chef deployment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = '841def4b930fbe508bf53d6743d90f87e212753de0990bd7dd5c1d6c947aa11c504b1f300e7e1c2ab7dbbd75aaf03edb23646a2a9b67d55a4299b3daee5c13e7'
